aaf7cf918f88c0042d7c890af157557e49f46043
maurosil015 <maurosil015@gmail.com>
2023-10-25 22:05:30 +0200

    septima subida..

---
+++ Funciones1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Funciones1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Hacer una función llamada “producto” que reciba dos números enteros y que devuelva el producto de ambos.
+            // Luego hacer un programa que pida el precio de un artículo y la cantidad vendida y muestre por pantalla el monto total a pagar. 
+            // Usar la función.
+            int precio, cantidad, total;
+
+            Console.WriteLine("ingrese el precio del articulo");
+            precio = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese la cantidad vendida");
+            cantidad = int.Parse(Console.ReadLine());
+
+            total = producto(precio, cantidad);
+
+            Console.WriteLine("El total a pagar es: " + total);
+
+        }
+        static int producto(int n1, int n2){
+            int r;
+            r = n1 * n2;
+            return r;
+        }
+    }
+}
